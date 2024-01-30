a351e4fd2ddd4a8b141dcffcba742840a5c89938
Gustavo Takao <gustavo_hideo@hotmail.com>
2024-01-30 08:20:45 -0300

    first commit

---
+++ pythongol_output.py
@@ -0,0 +1,31 @@
+a = 1 
+b = 2 
+i = 0 
+nome = 'Carlos' 
+t = 4 
+
+
+c = a + b 
+d = a - b 
+e = b * i 
+f = b / a 
+print ( c ) 
+print ( 'ola mundo' ) 
+
+
+carros = [ 'ford' , 'fiat' , 'nissan' ] 
+for carro in carros : 
+	print ( carro ) 
+
+while t < a: 
+	print ( 'a menor que b' ) 
+	a = 3 
+
+def ola ( nome ) : 
+	str = 'Ola' + nome + 'seja bem vindo!' 
+	return str 
+
+if a < b : 
+	z = 2 
+
+print ( z )  
